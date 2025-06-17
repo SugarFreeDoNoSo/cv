cba6fb64cd209f1022cdc254b2965a0b88a23910
Matias <matias@MacBook-Air-de-Matias.local>
2025-06-16 20:34:19 -0400

    SugarFreeDoNoSo me llamo

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://USERNAME.github.io",
+  site: "https://SugarFreeDoNoSo.github.io",
   base: "/cv",
   integrations: [preact()],
 
