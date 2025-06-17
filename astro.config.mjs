c001c9b27206fb95e254ade9bea55abbf4a477e4
Matias <matias@MacBook-Air-de-Matias.local>
2025-06-16 20:32:30 -0400

    page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://USERNAME.github.io",
+  base: "/cv",
   integrations: [preact()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
